d148b5bc2d92286ad65133cf02de7424687cda38
JohnM <mars.john2018@gmail.com>
2022-07-05 20:31:47 -0400

    fourth commit

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+        <div>
+            <p className="beats-solo">SMALL TEXT</p>
+        </div>
+    </div>
+  )
+}
+
+export default HeroBanner
